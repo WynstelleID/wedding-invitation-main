9561f63562b59370e59c815f7f08d8881ba85a6c
WynstelleID <setiyawandandi@gmail.com>
2025-02-17 15:33:16 +0700

    first commit

---
+++ src/assets/data/data.js
@@ -0,0 +1,127 @@
+export const data = {
+  bride: {
+    L: {
+      id: 1,
+      name: "Raden Ahmad Badaruddin",
+      child: "Putra ke 1",
+      father: "bapaknya ahmad",
+      mother: "tante netty",
+      image: "./src/assets/images/cowo.png",
+    },
+    P: {
+      id: 2,
+      name: "Dhanika Assandini",
+      child: "Putri ke 1",
+      father: "bapaknya dhanika",
+      mother: "Ipsum",
+      image: "./src/assets/images/cewe.png",
+    },
+
+    couple: "./src/assets/images/couple.png",
+  },
+
+  time: {
+    marriage: {
+      year: "2026",
+      month: "Januari",
+      date: "1",
+      day: "Senin",
+      hours: {
+        start: "08.00",
+        finish: "Selesai",
+      },
+    },
+    reception: {
+      year: "2026",
+      month: "Januari",
+      date: "2",
+      day: "Selasa",
+      hours: {
+        start: "11.00",
+        finish: "Selesai",
+      },
+    },
+    address:
+      "The Ritz-Carlton Jakarta, Jl. DR. Ide Anak Agung Gde Agung No.1 Kav.E.1.1, Mega Kuningan, Daerah Khusus Ibukota Jakarta 12950",
+  },
+
+  link: {
+    calendar: "https://calendar.app.google/oSVLRMYC79GzuA4f9",
+    map: "https://www.google.com/maps/dir/-6.2748246,106.8268098/hotel+ritz+carlton/@-6.2534754,106.8076584,14z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x2e69f3e53dc5171b:0x27a868ad3cd40c7d!2m2!1d106.827234!2d-6.2289029?entry=ttu&g_ep=EgoyMDI1MDIxMi4wIKXMDSoASAFQAw%3D%3D",
+  },
+
+  galeri: [
+    {
+      id: 1,
+      image: "./src/assets/images/1.png",
+    },
+    {
+      id: 2,
+      image: "./src/assets/images/2.png",
+    },
+    {
+      id: 3,
+      image: "./src/assets/images/3.png",
+    },
+    {
+      id: 4,
+      image: "./src/assets/images/4.png",
+    },
+    {
+      id: 5,
+      image: "./src/assets/images/5.png",
+    },
+  ],
+
+  bank: [
+    {
+      id: 1,
+      name: "BCA",
+      icon: "./src/assets/images/bca.png",
+      rekening: "12345678",
+    },
+    {
+      id: 2,
+      name: "BNI",
+      icon: "./src/assets/images/bri.png",
+      rekening: "12345678",
+    },
+  ],
+
+  audio: "./src/assets/audio/wedding.mp3",
+
+  api: "https://script.google.com/macros/s/AKfycbyg6oxz5o9gKN_iw5qBSlxOsdAYrGGu_LDLTiY8W2SUpAiGNJ781inlI1DFQ86ULDhe/exec",
+
+  navbar: [
+    {
+      id: 1,
+      teks: "Home",
+      icon: "bx bxs-home-heart",
+      path: "#home",
+    },
+    {
+      id: 2,
+      teks: "Mempelai",
+      icon: "bx bxs-group",
+      path: "#bride",
+    },
+    {
+      id: 3,
+      teks: "Tanggal",
+      icon: "bx bxs-calendar-check",
+      path: "#time",
+    },
+    {
+      id: 4,
+      teks: "Galeri",
+      icon: "bx bxs-photo-album",
+      path: "#galeri",
+    },
+    {
+      id: 5,
+      teks: "Ucapan",
+      icon: "bx bxs-message-rounded-dots",
+      path: "#wishas",
+    },
+  ],
+};
